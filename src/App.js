392a0baa7721518ab27e55bab54320359572f494
PMS <parkms6490@gmail.com>
2023-08-22 18:28:13 +0900

    test1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+/* eslint-disable */ 
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // state 언제 써야함?
+  // 변동 시 자동으로 html에 반영되게 만들고 싶으면 state 씀
+  // 자주 변경될거같은 html 부분은 state로 만들어놓기
+  const [postTitle, b] = useState(['동명지하', '형제네생갈비', '키키키']);
+  const [logo, setLogo] = useState('동명동 맛집 Blog');
+  const [like, likeTrans] = useState(0);
+
+  let likeCount = () => {
+    likeTrans(like + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <drag> 
+      <div className='black-nav'>
+        <h4>{ logo }</h4>
+      </div>
+      <div className='list'>
+        <h4>{ postTitle[0] } <span onClick={likeCount} >👍</span> {like} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ postTitle[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ postTitle[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      </drag>
     </div>
   );
 }
